e308366723bd14bc5072da45273ece13e88c52e2
oonisim <oonisim@gmail.com>
2021-10-01 18:56:23 +1000

    Update README.md

--- README.md
+++ README.md
@@ -6,4 +6,12 @@
 
 Starting point to learn Terraform.
 
-<img src="image/hashicorp_learn_terraform.png" align="left"/>
+<img src="image/hashicorp_learn_terraform.png" align="left" width="800"/>
+
+### HCL
+
+* [HashiCorp Learn - Teraform - Configuration Language](https://learn.hashicorp.com/collections/terraform/configuration-language)
+
+### Use Cases
+
+* [Manage Kubernetes with Terraform](https://learn.hashicorp.com/collections/terraform/kubernetes)
--- basics/terraform_configuration_block/versions.tf
+++ basics/terraform_configuration_block/versions.tf
@@ -1,6 +1,21 @@
 #--------------------------------------------------------------------------------
 # [terraform configuration block]
 # https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
+#
+# Lock and Upgrade Provider Versions
+# https://learn.hashicorp.com/tutorials/terraform/provider-versioning?in=terraform/configuration-language
+# When multiple users or automation tools run the same Terraform configuration,
+# they should all use the same versions of their required providers.
+# There are two ways for you to manage provider versions in your configuration.
+# 1. Specify provider version constraints in your configuration's terraform block.
+# 2. Use the dependency lock file
+#
+# [terraform.lock.hcl]
+# When you initialize a Terraform configuration for the first time with Terraform
+# 0.14 or later, Terraform will generate a new .terraform.lock.hcl file in the
+# current working directory. You should include the lock file in your version
+# control repository to ensure that Terraform uses the same provider versions
+# across your team and in ephemeral remote execution environments.
 #--------------------------------------------------------------------------------
 terraform {
   #--------------------------------------------------------------------------------
